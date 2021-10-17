c36d8db982be7aa8c201f5e85f148b17ce54ea95
RechieKho <the.real.richie.kho@gmail.com>
2021-10-16 18:31:06 -0700

    Add docstring for error.py

--- Sugary/src/error/error.py
+++ Sugary/src/error/error.py
@@ -5,6 +5,9 @@ Provides function for error handling
 from ...shared import log
 
 def err(err_desc:str):
+    """
+    Print out an error and quit the program
+    """
     print(log.make_error(err_desc))
     exit(0)
 
